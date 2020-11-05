190bc696812241addc288042cf56312ba9ac602c
Alice <alicelu625@gmail.com>
2020-11-04 21:22:43 -0800

    Updates

---
+++ precache-manifest.d786cb45a3541ba8730271f0299f28b4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c069b46358f909452441252996c77bc",
+    "url": "/react-burger-builder/index.html"
+  },
+  {
+    "revision": "5cdc919caf25638045d1",
+    "url": "/react-burger-builder/static/css/3.e77a3791.chunk.css"
+  },
+  {
+    "revision": "c72297b7d7a8d08586b7",
+    "url": "/react-burger-builder/static/css/4.3d353b26.chunk.css"
+  },
+  {
+    "revision": "cb163cfb1a52465b9249",
+    "url": "/react-burger-builder/static/css/5.bf041cdb.chunk.css"
+  },
+  {
+    "revision": "2e48385f66c43f5ea078",
+    "url": "/react-burger-builder/static/css/main.5a6de374.chunk.css"
+  },
+  {
+    "revision": "b36577d62ebfea966eba",
+    "url": "/react-burger-builder/static/js/2.0376c360.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-burger-builder/static/js/2.0376c360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdc919caf25638045d1",
+    "url": "/react-burger-builder/static/js/3.036c5722.chunk.js"
+  },
+  {
+    "revision": "c72297b7d7a8d08586b7",
+    "url": "/react-burger-builder/static/js/4.1ec8d2f4.chunk.js"
+  },
+  {
+    "revision": "cb163cfb1a52465b9249",
+    "url": "/react-burger-builder/static/js/5.4d44a1d6.chunk.js"
+  },
+  {
+    "revision": "2e48385f66c43f5ea078",
+    "url": "/react-burger-builder/static/js/main.c3880de3.chunk.js"
+  },
+  {
+    "revision": "6437f93a60a49e5044a8",
+    "url": "/react-burger-builder/static/js/runtime-main.4ce65f84.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
